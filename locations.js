c15aba66a3554fe70b8c1f70f6b6a383141e226b
RuizJason81 <nosajziur@outlook.com>
2025-04-04 02:14:43 -0400

    Update locations.js
    Add new locations with real coordinates

--- locations.js
+++ locations.js
@@ -1,9 +1,107 @@
 const locations = [
   {
-    lat: 37.38311857298441,
-    lng: -5.990224048237083,
-    title: "Seville Cathedral, Alcázar and Archivo de Indias",
-    description: "Seville’s historic heart is a blend of cultures and time periods.",
-    link: "https://moneyoffthebooks.com/seville-cathedral"
+    lat: -7.607355,
+    lng: 110.203804,
+    title: "Borobudur Temple Compounds",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/borobudur-temple"
+  },
+  {
+    lat: -7.75222,
+    lng: 110.49167,
+    title: "Prambanan Temple Compounds",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/prambanan-temple"
+  },
+  {
+    lat: 48.804722,
+    lng: 2.121782,
+    title: "Palace and Park of Versailles",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/palace-and"
+  },
+  {
+    lat: 51.508530,
+    lng: -0.076132,
+    title: "Tower of London",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/tower-of"
+  },
+  {
+    lat: 51.499390,
+    lng: -0.127465,
+    title: "Palace of Westminster and Westminster Abbey including Saint Margaret’s Church",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/palace-of"
+  },
+  {
+    lat: 51.1740,
+    lng: -1.8218,
+    title: "Stonehenge, Avebury and Associated Sites",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/stonehenge-avebury"
+  },
+  {
+    lat: 39.8628,
+    lng: -4.0273,
+    title: "Historic City of Toledo",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/historic-city"
+  },
+  {
+    lat: 37.8916,
+    lng: -4.7739,
+    title: "Historic Centre of Cordoba",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/historic-centre"
+  },
+  {
+    lat: 40.58167,
+    lng: -4.12583,
+    title: "Monastery and Site of the Escurial, Madrid",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/monastery-and"
+  },
+  {
+    lat: 41.4036,
+    lng: 2.1744,
+    title: "Works of Antoni Gaudí",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/works-of"
+  },
+  {
+    lat: 37.17667,
+    lng: -3.59444,
+    title: "Alhambra, Generalife and Albayzín, Granada",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/alhambra-generalife"
+  },
+  {
+    lat: 50.941357,
+    lng: 6.958307,
+    title: "Cologne Cathedral",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/cologne-cathedral"
+  },
+  {
+    lat: -7.801389,
+    lng: 110.364778,
+    title: "The Cosmological Axis of Yogyakarta and its Historic Landmarks",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/the-cosmological"
+  },
+  {
+    lat: 29.36167,
+    lng: -98.48028,
+    title: "San Antonio Missions",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/san-antonio"
+  },
+  {
+    lat: 18.466667,
+    lng: -66.125,
+    title: "La Fortaleza and San Juan National Historic Site in Puerto Rico",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "https://moneyoffthebooks.com/la-fortaleza"
   }
 ];
