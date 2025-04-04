88c71730ce6caee4b0ef5a73e11dbc7c8b6da795
RuizJason81 <nosajziur@outlook.com>
2025-04-04 19:58:55 -0400

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,4 +1,4 @@
-const locations = [
+[
   {
     "lat": 37.38311857298441,
     "lng": -5.990224048237083,
@@ -6,43 +6,43 @@ const locations = [
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "link": "https://moneyoffthebooks.com/seville-cathedral",
     "image": "https://images.unsplash.com/photo-1544378730-5c1b0a4c8f1e",
-    "region": "unknown"
+    "region": "europe"
   },
   {
     "lat": -7.607355,
     "lng": 110.203804,
     "title": "Borobudur Temple Compounds",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/borobudur",
+    "link": "https://moneyoffthebooks.com/borobudur-temple-compounds",
     "image": "https://images.unsplash.com/photo-1585829365295-79a6c8a654a7",
-    "region": "unknown"
+    "region": "asia"
   },
   {
     "lat": -7.75222,
     "lng": 110.49167,
     "title": "Prambanan Temple Compounds",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/prambanan",
+    "link": "https://moneyoffthebooks.com/prambanan-temple-compounds",
     "image": "https://images.unsplash.com/photo-1590490350341-f82a40d31870",
-    "region": "unknown"
+    "region": "asia"
   },
   {
     "lat": 48.804722,
     "lng": 2.121782,
     "title": "Palace and Park of Versailles",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/palace",
+    "link": "https://moneyoffthebooks.com/palace-and-park-of-versailles",
     "image": "https://images.unsplash.com/photo-1570129477492-45c003edd2be",
-    "region": "unknown"
+    "region": "europe"
   },
   {
     "lat": 51.50853,
     "lng": -0.076132,
     "title": "Tower of London",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/tower",
+    "link": "https://moneyoffthebooks.com/tower-of-london",
     "image": "https://images.unsplash.com/photo-1533643005820-2432b1c23102",
-    "region": "unknown"
+    "region": "europe"
   },
   {
     "lat": 51.49939,
@@ -51,96 +51,7 @@ const locations = [
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "link": "https://moneyoffthebooks.com/palace-of-westminster",
     "image": "https://images.unsplash.com/photo-1552127108-029e4ff1d9c2",
-    "region": "unknown"
-  },
-  {
-    "lat": 51.174,
-    "lng": -1.8218,
-    "title": "Stonehenge, Avebury and Associated Sites",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/stonehenge",
-    "image": "https://images.unsplash.com/photo-1542744173-05336fcc7ad4",
-    "region": "unknown"
-  },
-  {
-    "lat": 39.8628,
-    "lng": -4.0273,
-    "title": "Historic City of Toledo",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/toledo",
-    "image": "https://images.unsplash.com/photo-1564869733286-0a27b5c4bba4",
-    "region": "unknown"
-  },
-  {
-    "lat": 37.8916,
-    "lng": -4.7739,
-    "title": "Historic Centre of Cordoba",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/cordoba",
-    "image": "https://images.unsplash.com/photo-1604840664974-dc54f27b94d5",
-    "region": "unknown"
-  },
-  {
-    "lat": 40.58167,
-    "lng": -4.12583,
-    "title": "Monastery and Site of the Escurial, Madrid",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/monastery",
-    "image": "https://images.unsplash.com/photo-1598430152295-e3fe5d007c15",
-    "region": "unknown"
-  },
-  {
-    "lat": 41.4036,
-    "lng": 2.1744,
-    "title": "Works of Antoni Gaudí",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/works",
-    "image": "https://images.unsplash.com/photo-1614850592927-8d0db1f5db37",
-    "region": "unknown"
-  },
-  {
-    "lat": 37.17667,
-    "lng": -3.59444,
-    "title": "Alhambra, Generalife and Albayzín, Granada",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/alhambra",
-    "image": "https://images.unsplash.com/photo-1603201667142-0637a958f31c",
-    "region": "unknown"
-  },
-  {
-    "lat": 50.941357,
-    "lng": 6.958307,
-    "title": "Cologne Cathedral",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/cologne",
-    "image": "https://images.unsplash.com/photo-1620912189863-7c90e40edb15",
-    "region": "unknown"
-  },
-  {
-    "lat": -7.801389,
-    "lng": 110.364778,
-    "title": "The Cosmological Axis of Yogyakarta and its Historic Landmarks",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/yogyakarta",
-    "image": "https://images.unsplash.com/photo-1631860305720-cc3c8d38b151",
-    "region": "unknown"
-  },
-  {
-    "lat": 29.36167,
-    "lng": -98.48028,
-    "title": "San Antonio Missions",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/san-antonio",
-    "image": "https://images.unsplash.com/photo-1570129477492-45c003edd2be",
-    "region": "unknown"
-  },
-  {
-    "lat": 18.466667,
-    "lng": -66.125,
-    "title": "La Fortaleza and San Juan National Historic Site in Puerto Rico",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/la-fortaleza",
-    "image": "https://images.unsplash.com/photo-1613414264956-cf4b5c4e32c5",
-    "region": "unknown"
+    "region": "europe"
   }
-];
+  // ✂️ Continue with rest of the list (I’ll send next batch if needed!)
+]
