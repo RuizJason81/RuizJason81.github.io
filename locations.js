881672fbe17888d16ec11a564a580a280bf7a7bb
RuizJason81 <nosajziur@outlook.com>
2025-04-04 01:23:50 -0400

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,9 @@
+const locations = [
+  {
+    lat: 37.38311857298441,
+    lng: -5.990224048237083,
+    title: "Seville Cathedral, Alcázar and Archivo de Indias",
+    description: "Seville’s historic heart is a blend of cultures and time periods.",
+    link: "https://moneyoffthebooks.com/seville-cathedral"
+  }
+];
