9fbcc9bfea9fe74badb831608f490338e111795a
RuizJason81 <nosajziur@outlook.com>
2025-04-04 14:10:16 -0400

    Update locations.js

--- locations.js
+++ locations.js
@@ -5,7 +5,7 @@ const locations = [
     "title": "Seville Cathedral, Alc\u00e1zar and Archivo de Indias",
     "description": "Seville\u2019s mix of Islamic, Gothic, and Renaissance influences makes it a city where history is alive in every courtyard and cobbled street.",
     "link": "https://moneyoffthebooks.com/seville-cathedral",
-    "image": "https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/6/66/Sevilla_Catedral_Giralda.JPG/640px-Sevilla_Catedral_Giralda.JPG",
+    "image": "https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/6/66/Sevilla_Catedral_Giralda.JPG/640px-Sevilla_Catedral_Giralda.jpg&il",
     "region": "europe"
   },
   {
@@ -14,7 +14,7 @@ const locations = [
     "title": "Borobudur Temple Compounds",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "link": "https://moneyoffthebooks.com/borobudur-temple",
-    "image": "https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Borobudur_Nothwest_View.jpg/640px-Borobudur_Nothwest_View.jpg",
+    "image": "https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Borobudur_Nothwest_View.jpg/640px-Borobudur_Nothwest_View.jpg&il",
     "region": "asia"
   },
   {
