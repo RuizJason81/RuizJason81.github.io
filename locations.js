1cd6f7832ab1f3a893c505e06116ac0a1732c2aa
RuizJason81 <nosajziur@outlook.com>
2025-04-04 20:09:30 -0400

    Update locations.js

--- locations.js
+++ locations.js
@@ -49,9 +49,98 @@
     "lng": -0.127465,
     "title": "Palace of Westminster and Westminster Abbey including Saint Margaret’s Church",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "link": "https://moneyoffthebooks.com/palace-of-westminster",
+    "link": "https://moneyoffthebooks.com/palace-of-westminster-and-westminster-abbey-including-saint-margarets-church",
     "image": "https://images.unsplash.com/photo-1552127108-029e4ff1d9c2",
     "region": "europe"
+  },
+  {
+    "lat": 51.174,
+    "lng": -1.8218,
+    "title": "Stonehenge, Avebury and Associated Sites",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/stonehenge",
+    "image": "https://images.unsplash.com/photo-1542744173-05336fcc7ad4",
+    "region": "europe"
+  },
+  {
+    "lat": 39.8628,
+    "lng": -4.0273,
+    "title": "Historic City of Toledo",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/historic-city-of-toledo",
+    "image": "https://images.unsplash.com/photo-1564869733286-0a27b5c4bba4",
+    "region": "europe"
+  },
+  {
+    "lat": 37.8916,
+    "lng": -4.7739,
+    "title": "Historic Centre of Cordoba",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/historic-centre-of-cordoba",
+    "image": "https://images.unsplash.com/photo-1604840664974-dc54f27b94d5",
+    "region": "europe"
+  },
+  {
+    "lat": 40.58167,
+    "lng": -4.12583,
+    "title": "Monastery and Site of the Escurial, Madrid",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/monastery-and-site-of-the-escurial",
+    "image": "https://images.unsplash.com/photo-1598430152295-e3fe5d007c15",
+    "region": "europe"
+  },
+  {
+    "lat": 41.4036,
+    "lng": 2.1744,
+    "title": "Works of Antoni Gaudí",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/works-of-antoni-gaudi",
+    "image": "https://images.unsplash.com/photo-1614850592927-8d0db1f5db37",
+    "region": "europe"
+  },
+  {
+    "lat": 37.17667,
+    "lng": -3.59444,
+    "title": "Alhambra, Generalife and Albayzín, Granada",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/alhambra",
+    "image": "https://images.unsplash.com/photo-1603201667142-0637a958f31c",
+    "region": "europe"
+  },
+  {
+    "lat": 50.941357,
+    "lng": 6.958307,
+    "title": "Cologne Cathedral",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/cologne-cathedral",
+    "image": "https://images.unsplash.com/photo-1620912189863-7c90e40edb15",
+    "region": "europe"
+  },
+  {
+    "lat": -7.801389,
+    "lng": 110.364778,
+    "title": "The Cosmological Axis of Yogyakarta and its Historic Landmarks",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/the-cosmological-axis-of-yogyakarta-and-its-historic-landmarks",
+    "image": "https://images.unsplash.com/photo-1631860305720-cc3c8d38b151",
+    "region": "asia"
+  },
+  {
+    "lat": 29.36167,
+    "lng": -98.48028,
+    "title": "San Antonio Missions",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/san-antonio-missions",
+    "image": "https://images.unsplash.com/photo-1570129477492-45c003edd2be",
+    "region": "america"
+  },
+  {
+    "lat": 18.466667,
+    "lng": -66.125,
+    "title": "La Fortaleza and San Juan National Historic Site in Puerto Rico",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "link": "https://moneyoffthebooks.com/la-fortaleza-and-san-juan-national-historic-site-in-puerto-rico",
+    "image": "https://images.unsplash.com/photo-1613414264956-cf4b5c4e32c5",
+    "region": "america"
   }
-  // ✂️ Continue with rest of the list (I’ll send next batch if needed!)
 ]
